2d0a9dc779eb78a777985ecb4484ecd0844693f3
Amar Yassa <amaryassa@yahoo.fr>
2019-05-31 01:31:30 +0200

    grid and breakpoint  with mixin SCSS for header

--- src/app/apropos/apropos.component.ts
+++ src/app/apropos/apropos.component.ts
@@ -8,13 +8,13 @@ import { Observer } from 'rxjs';
   styleUrls: ['./apropos.component.scss']
 })
 export class AproposComponent implements OnInit {
-  // public defaultValue = {
-  //   "title": "titre",
-  //    "description": `il faut écrire un descriptif de votre site/ entreprise `,
-  //   "button": "nom bouton",
-  //   "image1" :"https://www.decofinder.com/i/une/Promotion/28693956.jpg",
-  //   "image2" :"https://www.decofinder.com/i/une/CoupDeCoeur/28693955.jpg"
-  // };
+  public defaultValue = {
+    "title": "titre",
+     "description": `il faut écrire un descriptif de votre site/ entreprise `,
+    "button": "nom bouton",
+    "image1" :"https://www.decofinder.com/i/une/Promotion/28693956.jpg",
+    "image2" :"https://www.decofinder.com/i/une/CoupDeCoeur/28693955.jpg"
+  };
 
   public about: any = [];
   constructor(private _proposService: AproposService) { }
