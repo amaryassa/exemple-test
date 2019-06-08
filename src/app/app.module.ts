2705554acb11a2b93444134492b0c42b279aff70
Amar Yassa <amaryassa@yahoo.fr>
2019-06-08 22:41:55 +0200

    bloc article fini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { MaintenanceProductsComponent } from './maintenance-products/maintenance-products.component';
-import { MaintenanceProductComponent } from './maintenance-products/maintenance-product/maintenance-product.component';
 import { AproposComponent } from './apropos/apropos.component';
 import { SliderComponent } from './slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +13,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
-import { FeaturedProductComponent } from './featured-products/featured-product/featured-product.component';
 import { ReseauSocialComponent } from './reseau-social/reseau-social.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 
@@ -33,11 +31,9 @@ registerLocaleData(localeFr, 'fr');
     FooterComponent,
     ArticlesComponent,
     MaintenanceProductsComponent,
-    MaintenanceProductComponent,
     AproposComponent,
     SliderComponent,
     FeaturedProductsComponent,
-    FeaturedProductComponent,
     ReseauSocialComponent,
     NewsletterComponent,
 
